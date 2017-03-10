ec8db799e421369c3a4ca8fd2b718ca71698e38e
rmit-s3543729-yesmi-marasinghe <s3543729@student.rmit.edu.au>
2017-03-10 17:25:36 +1100

    created Junit testing classes

---
+++ src/Test/LoginTest.java
@@ -0,0 +1,29 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by yesmi on 10/03/2017.
+ */
+class LoginTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void loginMenu() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void loadCustomerInformation() {
+
+    }
+
+}
---
+++ src/Test/RegisterTest.java
@@ -0,0 +1,28 @@
+package Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by yesmi on 10/03/2017.
+ */
+class RegisterTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void registerMenu() {
+
+    }
+
+}
