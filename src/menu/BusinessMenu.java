c3774d9db13d065e090af84c2dfcd52584d73173
Gabrielle Chan <salufie@gmail.com>
2017-03-24 16:54:14 +1100

    refractored register and also changed business menu options

--- src/menu/BusinessMenu.java
+++ src/menu/BusinessMenu.java
@@ -15,7 +15,12 @@ public class BusinessMenu {
         System.out.println("|              menu                |");
         System.out.println("+----------------------------------+");
 
-        System.out.println("3. Log out");
+        System.out.println("1. Add Employee");
+        System.out.println("2. Add working dates/times");
+        System.out.println("3. View summaries of bookings");
+        System.out.println("4. View new Bookings");
+        System.out.println("5. Show worker availability");
+        System.out.println("6. Log out");
 
         /* debug purposes
         for(int i=0; i < login.businessList.size() ;i++){
@@ -30,7 +35,7 @@ public class BusinessMenu {
             System.out.print("Enter choice: ");
             int choice = reader.nextInt();
 
-            if(choice == 3){
+            if(choice == 6){
                 System.out.println("Successfully logged out of the system!");
                 System.exit(0);
             }
