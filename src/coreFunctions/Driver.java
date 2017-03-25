741ed8fe5673fa0b64b785b5700a723b9aaf52b9
Gabrielle Chan <s3605767@student.rmit.edu.au>
2017-03-25 16:37:35 +1100

    started error handling for add employee.

--- src/coreFunctions/Driver.java
+++ src/coreFunctions/Driver.java
@@ -16,54 +16,112 @@ public class Driver {
 
     public void addEmployee(){
         Scanner reader = new Scanner(System.in);
+
         boolean valid = true;
-    while(valid) {
         System.out.println("\nAdd Employee");
         System.out.println("====================");
 
+        //infinite loop
+    while(true) {
         String employeeID = generateEmployeeNo();
 
         /* debug */
         System.out.println("Employee ID is " + employeeID);
 
         //other stuff
+
         System.out.print("Enter full name: ");
         String employeeName = reader.nextLine();
 
+        //error check for length of name
+        employeeName = verifyEmployeeName(employeeName);
+
+
+
         System.out.print("Enter tax file number: ");
-        String tfn = reader.nextLine();
+
+        //checks if numbers are entered for tfn
+        while(!reader.hasNextInt()) {
+            System.out.println("\nError: entered a non integer as tfn.");
+            System.out.print("Enter tax file number: ");
+            reader.next();
+        }
+        int checkTfn = reader.nextInt();
+        checkTfn = verifyEmployeeTFN(checkTfn);
+        String tfn = Integer.toString(checkTfn);
+
+
         System.out.print("Enter phone number: ");
-        String phoneNo = reader.nextLine();
-        valid = VerifyEmployee(employeeName,tfn,phoneNo);
+
+        //checks if numbers are entered for phone no
+        while(!reader.hasNextInt()) {
+            System.out.println("\nError: a phone number consists of 10 digits.");
+            System.out.print("Enter phone number: ");
+            reader.next();
+        }
+
+        int checkPhone = reader.nextInt();
+        String phoneNo = Integer.toString(checkPhone);
+
+        valid = VerifyEmployee(tfn,phoneNo);
         if(valid){
             filewriter.WriteToEmployee(new Employee(employeeID, employeeName, tfn, phoneNo), "employeeList.txt");
-
             System.out.println("Successfully added a new employee");
+            break;
+        }
+        else{
+            System.out.println("\nPlease re-enter employee details.");
+            continue;
         }
     }
 
     }
-    public boolean VerifyEmployee(String name,String tfn, String mobile){
-        if(name.isEmpty() || (name.length() < 1)  ){
 
-            System.out.println("Invalid Name");
-            return true;
+    public String verifyEmployeeName(String name){
+        Scanner reader = new Scanner(System.in);
+        while(name.length()< 3){
+            System.out.println("Error: Name must be longer than 2 characters");
+            System.out.print("Re-enter full name: ");
+            name = reader.nextLine();
+        }
+        return name;
+    }
 
+    public int verifyEmployeeTFN(int tfn){
+        Scanner reader = new Scanner(System.in);
+
+        int length = (int) Math.log10(tfn) + 1;
+        if((length != 8) || (length != 9)){
+            System.out.println("Please re-enter");
+
+            while(!reader.hasNextInt()) {
+                System.out.println("\nError: entered a non integer as tfn.");
+                System.out.print("Enter tax file number: ");
+                reader.next();
+            }
+            tfn = reader.nextInt();
 
         }
+
+
+
+        return tfn;
+    }
+
+    public boolean VerifyEmployee(String tfn, String mobile){
 
         if(tfn.isEmpty() || (tfn.length() != 8)||(tfn.length() != 9) ){
             System.out.println("Invalid TFN");
-            return true;
+            return false;
 
         }
 
         if(mobile.isEmpty() || (mobile.length() != 10) ){
             System.out.println("Invalid Mobile");
-            return true;
+            return false;
 
         }
-        return false;
+        return true;
     }
     /*
      * Generate employee ID
