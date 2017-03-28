bd5d73f4d1e16cc26399b63342d3bd6a73f3e23a
rmit-s3543729-yesmi-marasinghe <s3543729@student.rmit.edu.au>
2017-03-28 22:44:05 +1100

    time to tomorrow

--- src/coreFunctions/WriteToFile.java
+++ src/coreFunctions/WriteToFile.java
@@ -5,7 +5,6 @@ import user.Employee;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
 
 /**
  * Created by Martin on 5/03/2017.
--- src/menu/BusinessMenu.java
+++ src/menu/BusinessMenu.java
@@ -1,11 +1,18 @@
 package menu;
-import BusinessWorkDays.Workday;
-import coreFunctions.Driver;
-import user.Business;
 
+import coreFunctions.Driver;
+import user.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Created by Gabrielle on 5/03/2017.
@@ -14,27 +21,15 @@ import java.util.*;
 public class BusinessMenu {
     Login login = new Login();
     Driver driver = new Driver();
-    String starttime;
-    String endtime;
+    public static ArrayList<Employee> employeeList = new ArrayList<>();
 
-
-
-
-
-
-    public void printMenu(String bID){
+    public void printMenu(String bId){
         Scanner reader = new Scanner(System.in);
-        Scanner hours = new Scanner(System.in);
-        Scanner workdayreader = new Scanner(System.in);
-        int command;
-        GregorianCalendar cur = new GregorianCalendar();
         Scanner eID = new Scanner(System.in);
-        String firstdate;
+        String starttime;
+        String endtime;
+        String day;
         String empID;
-
-        ArrayList<String> workday = new ArrayList<>(7);
-        ArrayList<String> chosendays = new ArrayList<>(7);
-
 
 
 
@@ -48,15 +43,6 @@ public class BusinessMenu {
 
         //infinite loop
         while(true) {
-            /* adds into arraylist */
-            workday.add("Monday");
-            workday.add("Tuesday");
-            workday.add("Wednesday");
-            workday.add("Thursday");
-            workday.add("Friday");
-            workday.add("Saturday");
-            workday.add("Sunday");
-
             //print business menu
             System.out.println("\n+----------------------------------+");
             System.out.println("|           Business               |");
@@ -65,123 +51,54 @@ public class BusinessMenu {
 
             System.out.println("1. Add Employee");
             System.out.println("2. Add working days/times");
-            System.out.println("3. View summaries of bookings");
-            System.out.println("4. View new Bookings");
-            System.out.println("5. Show worker availability");
-            System.out.println("6. Log out");
+            System.out.println("3. Remove working days/times");
+            System.out.println("4. View summaries of bookings");
+            System.out.println("5. View new Bookings");
+            System.out.println("6. Show worker availability");
+            System.out.println("8. Log out");
 
             System.out.print("Enter choice: ");
             int choice = reader.nextInt();
 
             if(choice == 1){
-                driver.addEmployee();
+                driver.addEmployee(bId);
                 continue;
             }
 
             if(choice == 2){
                 boolean valid = true;
-                boolean validwork = true;
-
 
                 while(valid){
 
-                    while(true){
-                        try{
-                            System.out.println("Enter Start Time");
-                            starttime = hours.nextLine();
-
-                            if(Integer.parseInt(starttime) > 24 ||Integer.parseInt(starttime) <0){
-                                System.out.println("Invalid Start Times");
-                                continue;
-                            }else{
-                                System.out.println("Enter End Time");
-                                endtime = hours.nextLine();
-                                if(Integer.parseInt(endtime) <0 || Integer.parseInt(endtime) > 24 || Integer.parseInt(endtime) < Integer.parseInt(starttime)){
-                                    System.out.println("Invalid Start Times");
-                                    continue;
-                                }
-                            }
-
-                            break;
-                        }
-                        catch (NumberFormatException e){
-                            System.out.println("Invalid Times");
-
-                        }
-
-
-
-
-                    }
-
-
-                    while(validwork){
-
-                        for(int i=0; i< workday.size(); i++){
-                            System.out.println((i+1) + ". " + workday.get(i));
-
-                        }
-                        if(workday.size() == 0){
-                            break;
-                        }
-                        System.out.println("0. Exit");
-
-                        command = workdayreader.nextInt();
-                        /*
                         do{
                         System.out.print("Enter employee ID:");
                         empID = eID.nextLine();
-                        }while(!checkEmployeeID(empID));
+                        }while(!checkEmployeeID(bId,empID));
+                    day = reader.nextLine();
+                        do {
+                            System.out.println("Enter Day:");
+                            day = reader.nextLine();
+                        }while(checkDay(day));
+                    do {
+                        System.out.print("Enter shift start time:");
+                        starttime = reader.nextLine();
+                    }while(checktime(starttime));
+                    do {
+                        System.out.print("Enter shift end time:");
+                        endtime = reader.nextLine();
+                    }while(checktime(endtime));
 
-*/
-                        if(command == 0 && workday.size() < 7){
-                            validwork = false;
-                        }
+                    valid = Worktimes(bId,empID, day,starttime,endtime);
 
-                        else if(command == 1 ){
-                            chosendays.add(workday.get(0));
-                            workday.remove(0);
-
-                        }
-                        else if(command == 2  && workday.size()>=2 ){
-                            chosendays.add(workday.get(1));
-                            workday.remove(1);
-                        }
-                        else if(command == 3  && workday.size()>=3 ){
-                            chosendays.add(workday.get(2));
-                            workday.remove(2);
-                        }
-                        else if(command == 4 && workday.size()>=4 ){
-                            chosendays.add(workday.get(3));
-                            workday.remove(3);
-                        }
-                        else if(command == 5 && workday.size()>=5){
-                            chosendays.add(workday.get(4));
-                            workday.remove(4);
-                        }
-                        else if(command == 6 && workday.size()>=6){
-                            chosendays.add(workday.get(5));
-                            workday.remove(5);
-                        }
-                        else if(command == 7 && workday.size()>=7){
-                            chosendays.add(workday.get(6));
-                            workday.remove(6);
-                        }
-                        else{
-                            System.out.println("Invalid input");
-                        }
-
-                    }
-                    Workday createWorkdays = new Workday(bID, chosendays,starttime,endtime);
-
-                    createWorkdays.writeToFile();
-                    workday.clear();
-                    break;
                 }
+            }
+            if(choice == 8){
+                System.out.println("--------- New Service---------");
+                addNewService();
 
             }
 
-            else if(choice == 6){
+            else if(choice == 8){
                 System.out.println("Successfully logged out of the system!");
                 System.exit(0);
             }
@@ -196,56 +113,68 @@ public class BusinessMenu {
         }
 
     }
+    public void addNewService()
+    {
+        // owner adds the service
+        // name
+        // cost
+        // time length
+        // description
+        // this is sent to a text file to allow the customer to choose from service when booking
+    }
+public boolean checkD(String day){return checkDay(day);}
+    private boolean checkDay(String day){
+        try{
+            DateFormat time = new SimpleDateFormat("EEEE");
+            time.parse(day);
+        }
+        catch(ParseException e){
+            System.out.println("Invalid Day");
+            return true;
 
+        }
+        return false;
+    }
+    private boolean checktime(String t){
+        try{
+            DateFormat time = new SimpleDateFormat("HH:mm:ss");
+            time.parse(t);
+        }
+        catch(ParseException e){
+            System.out.println("Invalid time:");
+            return true;
 
-    private boolean Worktimes(String firstdate,String starttime,String endtime){
-        SimpleDateFormat dateformat2 = new SimpleDateFormat("dd/MM/yyyy|HH:mm:ss");
-
-        String dateNtime = firstdate + "|"+ starttime;
-        String endNTime = firstdate + "|" + endtime;
+        }
+        return false;
+    }
+    private boolean Worktimes(String bId, String empId, String day,String starttime,String endtime){
+        DateFormat time = new SimpleDateFormat("HH:mm:ss");
 
         try{
-            Date currDate =  new Date();
-            Date startdate = dateformat2.parse(dateNtime);
-            Date enddate = dateformat2.parse(endNTime);
 
-            long diff = enddate.getTime() - startdate.getTime();
-            long diffHours = diff / (60 * 60 * 1000) % 24;
-            long diffMinutes = diff / (60 * 1000) % 60;
-            long diffHM = (diffHours*60)+diffMinutes;
+            Date st = time.parse(starttime);
+            Date et = time.parse(endtime);
 
-                        /* If needed
-                        long diffSeconds = diff / 1000 % 60;
-                        long diffMinutes = diff / (60 * 1000) % 60;
-                        long diffDays = diff / (24 * 60 * 60 * 1000);
-                        */
 
             // This makes sure scheduled work day CANNOT be before the current time and date, Ending work time must not be before start time or equal.
-            if(startdate.before(currDate)){
-
-                System.out.println("Going back in time");
-                return true;
-            }else if(!enddate.after(startdate)){
+            if(!et.after(st)){
                 System.out.println("Can't Start after its ended");
                 return true;
 
-            }else if(startdate.equals(enddate)){
+            }else if(st.equals(et)){
                 System.out.println("Can't Start and end at same time");
                 return true;
             }
-
-            if(diffHours == 0 || diffHours > 8|| diffHM>480){
-                System.out.println("selected "+diffHours + " hours and "+diffMinutes+" minutes");
-                System.out.println("Work day must be in range between 1 to 8 hours");
-                return true;
-
-            }
+         /*   if(//call to method ){
+         System.out.println("This is an invalid shift");
+         return true;
+         }
+         */
 
 
-            //System.out.println(diffHours);
-            System.out.println("The working time of:\t\t\t " + startdate + "\nTil:\t\t\t\t\t\t\t " + enddate);
+            System.out.println("The working time of: " + day + ":  "+starttime+" - " + endtime);
 
-            driver.addWorkdays(dateNtime, endNTime);
+            driver.addWorkdays(bId,empId,day,starttime,endtime);
             return false;
 
         }catch(ParseException e){
@@ -253,17 +182,56 @@ public class BusinessMenu {
             return true;
 
         }
+        //CHECK AGAINST TEXTFILE WITH DAY RESTRICTIONS SET BY BUSINESS
     }
-    public  boolean Workt(String firstdate,String firsttime,String endtime){return Worktimes(firstdate,firsttime,endtime);}
+    public  boolean Workt(String bId,String empId, String day,String starttime,String endtime){return Worktimes(bId,empId,  day, starttime, endtime);}
+    public void loadCustomerInformation(){
+        BufferedReader br;
+        try {
 
 
-    public boolean checkEmployeeID(String empID){
+            br = new BufferedReader(new FileReader("employeeList.txt"));
 
-        if (empID.equals("e1") || empID.equals("e2")){
-            return true;
+            try {
+                String x;
+                while ( (x = br.readLine()) != null ) {
+                    // printing out each line in the file
+                    String loginDetails[] = x.split(":",5);
+                    String bId = loginDetails[0];
+                    String empID = loginDetails[1];
+                    String fullName = loginDetails[2];
+                    String TFN = loginDetails[3];
+                    String phoneNo = loginDetails[4];
+                    Employee e = new Employee(bId, empID, fullName, TFN, phoneNo);
+                    employeeList.add(e);
+                }
+                //prints error
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            //file cannot be found
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            e.printStackTrace();
         }
 
-        else
+    }
+
+    public boolean checkEmployeeID(String bId,String empID){
+
+        loadCustomerInformation();
+
+            for(int i=0; i < employeeList.size() ;i++){
+                if(empID.equals(employeeList.get(i).geteId())){
+                    if(bId.equals(employeeList.get(i).getbId())){
+                        return true;
+                    }
+                }
+
+            }
+            System.out.println("employee ID invalid");
             return false;
 
     }
