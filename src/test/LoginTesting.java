fe80b24e23abba0d9f7917da09e88a47fc4afc79
rmit-s3543729-yesmi-marasinghe <s3543729@student.rmit.edu.au>
2017-04-21 21:58:04 +1000

    added tests to login

--- src/test/LoginTesting.java
+++ src/test/LoginTesting.java
@@ -17,7 +17,7 @@ public class LoginTesting {
         log.loadCustomerInformation();
         log.getOwnerinfo();
     }
-    ////////////////////////////////////user testing
+    //---------------------------------------------------------- customer testing
     @Test
     public void verifyLoginAll() {
         toVerify =  login.getVerification("customer","c1","123");
@@ -34,30 +34,56 @@ public class LoginTesting {
         toVerify =  login.getVerification("c","c0000000000000000000000000000000001","123");
         assertFalse(toVerify);
     }
+    @Test
+    public void IncorrectUNBoundary2() {
+        toVerify =  login.getVerification("c","c0000000000abc000000000000000000000001","123");
+        assertFalse(toVerify);
+    }
 
     @Test
     public void IncorrectPW() {
         toVerify =  login.getVerification("customer","c1","c");
         assertFalse(toVerify);
     }
-    ////////////////////////////////////business owner testing
+
+    //---------------------------------------------------business owner testing
     @Test //invalid Type + user Details
     public void verifyLoginAllB() {
         toVerify =  login.getVerification("business","b1","123");
         assertTrue(toVerify);
     }
-
     @Test
     public void IncorrectUNB() {
         toVerify =  login.getVerification("business","b1notrealusername","123");
         assertFalse(toVerify);
     }
 
+    @Test //invalid Type + correct user Details
+    public void IncorrectUNB2() {
+        toVerify =  login.getVerification("business","b1notrealus00000000000000000000ername","123");
+        assertFalse(toVerify);
+    }
+    @Test
+    public void IncorrectUNBBoundary() {
+        toVerify =  login.getVerification("business","b0000000000000000000000000000000001","123");
+        assertFalse(toVerify);
+    }
+    @Test
+    public void IncorrectUNBBoundary2() {
+        toVerify =  login.getVerification("business","b0000000000abc000000000000000000000001","123");
+        assertFalse(toVerify);
+    }
     @Test
     public void IncorrectPWB() {
         toVerify =  login.getVerification("business","b1","fakepassword");
         assertFalse(toVerify);
     }
+    @Test
+    public void IncorrectPWB2() {
+        toVerify =  login.getVerification("business","b1","ccccccccccccccccccccccccccccccccccccccc");
+        assertFalse(toVerify);
+    }
+
 
 
 }
