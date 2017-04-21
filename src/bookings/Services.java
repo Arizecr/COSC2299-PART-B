0a961604311aa6d9b1abc00180cf8c7f2cf17bec
rmit-s3543729-yesmi-marasinghe <s3543729@student.rmit.edu.au>
2017-04-21 15:31:24 +1000

    services removal

--- src/bookings/Services.java
+++ src/bookings/Services.java
@@ -46,6 +46,10 @@ public class Services {
                 serviceList = new ArrayList<>();
                 ArrayList<String> EOserviceList = new ArrayList<>();
                 String x;
+                String line = "|Service ID|Name of Service | length: in Hours and Minutes" ;
+                System.out.println(line);
+
+
                 while ((x = br.readLine()) != null) {
                     EOserviceList = new ArrayList<>();
                     // printing out each line in the file
@@ -55,14 +59,22 @@ public class Services {
                     String n = Details[2];
                     String l = Details[3];
                     String e = Details[4];
+                    String Time[] = l.split("-", 2);
+                    String hours = Time[0];
+                    String min = Time[1];
                     String empID[] = e.split(",");
                     for(String emp:empID){
                         EOserviceList.add(emp);
                     }
                     Services addS = new Services(bid,sid,n,l,EOserviceList);
                     serviceList.add(addS);
-                    if(bid.equals(b)&&type.equals("b")){System.out.println(sid + " | " + n + " | " + l+" | "+e);}
-                    if(bid.equals(b)&&type.equals("c")){System.out.println(sid + " | " + n + " | " + l);}
+
+                        line = "|"+ sid + " |" + n + " |" + hours+" +"+min;
+                        line = String.format("|%10s|%16s|%2s hours and %s minutes" ,sid,n,hours,min);
+
+                        System.out.println(line);
+
+
 
                 }
                 //prints error
@@ -89,7 +101,7 @@ public class Services {
     }
     public void addService(String b){//,String sId,String name, String time
         ArrayList<String> EOserviceList = new ArrayList<>();
-       String s = generateServiceNo(); //generate user id
+        String s = generateServiceNo(); //generate user id
         int sSize = serviceList.size();
         WriteToFile w = new WriteToFile();
         String n = null;
@@ -99,9 +111,11 @@ public class Services {
         do {
             System.out.print("Name of service: ");
             n = reader.nextLine();
-            System.out.print("length in HH:mm ");
+        }while(!checkName(n));
+        do {
+            System.out.print("length in HH-mm ");
             len = reader.nextLine();//----------------------------------- checking boolean
-        }while(!checkService(n,len));
+        }while(!checkDur(len));
         do {
             System.out.println("employee IDs in the form[eX,eX,eX,...]: ");
             employees = reader.nextLine();
@@ -127,7 +141,7 @@ public class Services {
             System.out.print("Service ID: ");
             n = reader.nextLine();
             index = checkID(n);
-           }while(index==0);
+        }while(index==0);
 
         serviceList.remove(index-1);
         rewriteToFile(serviceList,"services.txt");
@@ -169,7 +183,7 @@ public class Services {
     public int checkID(String n){
         for(int i=0; i < serviceList.size() ;i++){
             if(n.equals(serviceList.get(i).sId)){
-              return i+1;
+                return i+1;
             }
         }
 
@@ -177,23 +191,65 @@ public class Services {
     }
     public int checkEID(int s,String e){
 
-            ArrayList<String> emplist = serviceList.get(s).emp;
-            for(int j=0; j < emplist.size() ;j++){
-                if(e.equals(emplist.get(j))){
-                    return j+1;//never returns zero
-                }
+        ArrayList<String> emplist = serviceList.get(s).emp;
+        for(int j=0; j < emplist.size() ;j++){
+            if(e.equals(emplist.get(j))){
+                return j+1;//never returns zero
+            }
 
         }
 
         return 0;
     }
-    public boolean checkService(String n, String l){
-        if(n.length()>15){
-            System.out.println("name is too long(must be less than 15 characters)");
+    public boolean checkName(String n){
+        if(n.length()>16){
+            System.out.println("name is too long(must be less than 16 characters)");
             return false;
         }
-        //if(l)//not int or not correct format
-     return true;
+        if(n.length()<=5){
+            System.out.println("name is too short(must be more than 5 characters)");
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean checkDur(String n){
+        String Details[] = n.split("-", 2);
+        String hours = Details[0];
+        String min = Details[1];
+        int h = 0;
+        int m =0;
+        try{
+            h = Integer.parseInt(hours);
+        } catch (NumberFormatException e) {
+            System.out.println("invalid hours");
+            return false;
+        }
+        try{
+            m = Integer.parseInt(min);
+        } catch (NumberFormatException e) {
+            System.out.println("invalid minutes");
+            return false;
+        }
+        if(n.length()>5){
+            System.out.println("invalid length");
+            return false;
+        }
+        if(h>8||(h==8&&m>0)){
+            System.out.println("cannot be longer than 8 hours");
+            return false;
+        }
+        if(m!=0&&m!=30){
+            System.out.println("minutes can only be 0 or 30");
+            return false;
+        }
+        if(h==0&&m==0){
+            System.out.println("length of service cannot be 0");
+            return false;
+        }
+
+        return true;
     }
     /*
     * Generate employee ID
