d3c61d0dbfda0e6e5940631871bdd5707d2e1b10
rmit-s3543729-yesmi-marasinghe <s3543729@student.rmit.edu.au>
2017-04-22 15:19:35 +1000

    fixed issue with customer menu

--- src/menu/CustomerMenu.java
+++ src/menu/CustomerMenu.java
@@ -55,7 +55,8 @@ public class CustomerMenu {
                     continue;
                 case 2:
                     System.out.println("Current Bookings: ");
-                    String b = "b"+Integer.toString(bID);
+                    String b = "b"+Integer.toString(bID+1);
+                    System.out.println(b);
                     driver.viewBookingsCustomer(username,b); //view current bookings
                     continue;
                 case 3:
