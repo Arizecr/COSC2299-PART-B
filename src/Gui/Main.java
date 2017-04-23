8516589793653f45cf5d0faca9cc0f1a4c4206f9
Gabrielle Chan <s3605767@student.rmit.edu.au>
2017-04-24 09:50:20 +1000

    made small changes to main menu and register

--- src/Gui/Main.java
+++ src/Gui/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Booking System Application");
-        primaryStage.setScene(new Scene(root, 500, 400));
+        primaryStage.setScene(new Scene(root, 480, 350));
         primaryStage.show();
 
     }
