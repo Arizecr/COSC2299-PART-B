6ba204e85c3f25081588a04882cdfad12273ec77
rmit-s3543729-yesmi-marasinghe <s3543729@student.rmit.edu.au>
2017-04-28 20:25:02 +1000

    small change in booking

--- src/Gui/customerMenu/bookingController.java
+++ src/Gui/customerMenu/bookingController.java
@@ -1,7 +1,23 @@
 package Gui.customerMenu;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * Created by yesmi on 28/04/2017.
  */
 public class bookingController {
+    public void cancel(ActionEvent event) throws IOException {
+        Parent home_page = FXMLLoader.load(getClass().getResource("customerMenu.fxml"));
+        Scene home_page_scene = new Scene(home_page);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
+    }
 }
